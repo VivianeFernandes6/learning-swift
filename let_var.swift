cc9649a36ed741d645b5f247cb563311c313e167
Vivi <70662490+VivianeFernandes6@users.noreply.github.com>
2026-07-13 17:17:48 -0300

    Add files via upload

--- let_var.swift
+++ let_var.swift
@@ -1,12 +1,42 @@
-// definir variáveis, eu uso o "var":
-var idade = 29
-print(idade)
+import UIKit
 
-idade = 24
-print(idade)
+"""
+para criar uma variável:
+1. eu utilizo a keyword var
+2. dou um espaço e digito o nome que eu quero para essa variável
+3. atribuo valor a esta variável usando =
+4. dou um espaço e digito o valor que quero atribuir a esta variável
+"""
+var greeting = "Hello, playground"
+// visualizo valor da variável, imprimindo na tela
+print(greeting)
 
-// definir constantes, eu uso o "let":
-let nome = "Noemi"
-print(nome)
-// nome = "Saulo" -> isso aqui não pode ser feito, já que nome, foi definido como constante e constantes não podem ser alteradas.
-print(nome)
+// como é uma VARIÁVEL
+var name = "Vania"
+print(name)
+// eu posso alterar o valor dela
+name = "Tom"
+print(name)
+
+var lastName: String = "Araújo"
+print(lastName)
+
+lastName = "Souza"
+print(lastName)
+
+// eu posso ter dois códigos diferentes na mesma linha, só separando por ;
+// apesar de que é muito mais limpo (do meu ponto de vista) pular linha, mas é uma alternativa
+var idade: Int = 24; var idade2: Int = 23
+print(idade)
+print(idade2)
+
+
+let address = "Rua das Flores, 123"
+print(address)
+
+let isBeautiful: Bool = true
+print(isBeautiful)
+
+var hairColor: String = "Ruivo"
+print(hairColor)
+
