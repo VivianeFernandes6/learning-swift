87cba860447403690dde9cb23c30035e1e918a54
Vivi <70662490+VivianeFernandes6@users.noreply.github.com>
2026-09-17 11:16:37 -0300

    docs: add break in switch statement example

--- LogicAndProgramming04.swift
+++ LogicAndProgramming04.swift
@@ -383,3 +383,24 @@ for character in puzzleInput {
     puzzleOutput.append(character)
 }
 print(puzzleOutput)
+
+// break in a switch statement
+let letter: Character = "V"
+var possibleSound: String?
+switch letter {
+case "v", "V":
+    possibleSound = "Vê"
+case "b", "B":
+    possibleSound = "Bê"
+case "t", "T":
+    possibleSound = "Tê"
+case "s", "S":
+    possibleSound = "Essi"
+default:
+    break
+}
+if let letterSound = possibleSound {
+    print("The sound of \(letter) is \(letterSound).")
+} else {
+    print("The sound couldn't be found for \(letter).")
+}
